4ab9d4968065af78f04edac149d0e1b616186eeb
boulanouarwalid <walid.boulanouar@etu.uae.ac.ma>
2026-05-11 19:36:16 +0100

    wahd

--- src/main/java/com/controleproject/factory/TacheFactory.java
+++
@@ -1,9 +0,0 @@
-package com.controleproject.factory;
-
-import com.controleproject.entity.Projet;
-import com.controleproject.entity.Tache;
-import org.springframework.stereotype.Component;
-
-public interface TacheFactory {
-    Tache creerTache(String nom, Projet projet);
-}
--- src/main/java/com/controleproject/strategy/AvancementStrategy.java
+++
@@ -1,5 +0,0 @@
-package com.controleproject.strategy;
-
-public interface AvancementStrategy {
-    double calculer(double montantPrevu, double montantConsomme);
-}
